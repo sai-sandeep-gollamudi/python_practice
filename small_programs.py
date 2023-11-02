36f76ff807524dc3cd537100141aa8f3fd8c7e41
sai-sandeep-gollamudi <107009354+sai-sandeep-gollamudi@users.noreply.github.com>
2023-11-01 22:44:51 -0500

    Update small_programs.py

--- small_programs.py
+++ small_programs.py
@@ -35,3 +35,85 @@ travel_log.append({'Country':country_name,'Places_visited':places_visited,'Times
 print(travel_log)
 
 '''
+
+# avg temperature, and days which record temperature more than average
+days = int(input("Enter the number of days: "))
+c=1
+s=0
+l=[]
+while(c<=days):
+    temp = (int(input(f"Enter the temp of day {c}:  ")))
+    s+=temp
+    l.append(temp)
+    c+=1
+
+avg = round((s/days),2)
+print(f"The average temperature is: {avg}")
+print("The days where temperature is above average: ")
+for i in range(len(l)):
+    if(l[i]>avg):
+        print(f"day {i+1}")
+
+
+#max product of two integers in array
+def max_prod(arr):
+    arr = arr.sort(reverse=True)
+    return arr[0]* arr[1]
+
+#middle function which returns list without first and last elements
+def middle(lst):
+    # TODO
+    out = []
+    for i in range(1,len(lst)-1):
+        out.append(lst[i])
+    return out
+    #return lst[1:-1]
+
+#diagonal sum
+def diagonal_sum(matrix):
+    # TODO
+    l = len(matrix)
+    c=0
+    s=0
+    while(c<l):
+        s=s+matrix[c][c]
+        c+=1
+    return s
+
+#first and second best scores from list include duplicates
+def first_second(my_list):
+    # TODO
+    my_list.sort(reverse=True)
+    return my_list[0],my_list[1]
+
+
+#remove duplicates from list
+def remove_duplicates(arr):
+    # TODO
+    out = []
+    for i in range(len(arr)):
+        if arr[i] not in out:
+            out.append(arr[i])
+    return out
+
+#Write a function to find all pairs of an integer array whose sum is equal to a given number.
+#Do not consider commutative pairs.
+def pair_sum(myList, sum):
+    # TODO
+    out=[]
+    for i in range(len(myList)):
+        for j in range(i,len(myList)):
+            if(myList[i]+myList[j]==sum):
+                out.append(f"{myList[i]}+{myList[j]}")
+    return out
+
+#check if the list has duplicates
+
+def contains_duplicate(nums):
+    # TODO
+    seen = set()
+    for i in range(len(nums)):
+        if nums[i] in seen:
+            return True
+        seen.add(nums[i])
+    return False
