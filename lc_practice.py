dcb364c1f8c4ddf717c6513281c305f8126a1737
sai-sandeep-gollamudi <107009354+sai-sandeep-gollamudi@users.noreply.github.com>
2024-01-01 03:37:04 -0600

    Update lc_practice.py

--- lc_practice.py
+++ lc_practice.py
@@ -42,6 +42,41 @@ class Solution:
 '''
 #########################################################
 
+lc-75
+
+    def mergeAlternately(self, word1: str, word2: str) -> str:
+        l = min(len(word1), len(word2))
+        res = ""
+        for i in range(l):
+            res = res + word1[i] + word2[i]
+        if (l < len(word1)):
+            for i in range(l, len(word1)):
+                res = res + word1[i]
+        else:
+            for i in range(l, len(word2)):
+                res = res + word2[i]
+        return res
 
 
+    def gcdOfStrings(self, str1: str, str2: str) -> str:
+        l1,l2 = len(str1),len(str2)
+        for i in range(l2,0,-1):
+            if(l1%i==0 and l2%i==0):
+                mt1=l1//i
+                mt2=l2//i
+                temp=str2[:i]
+                if(str1==mt1*temp and str2==mt2*temp):
+                    return temp
+        temp=""
+        return temp
 
+    def kidsWithCandies(self, candies: List[int], extraCandies: int) -> List[bool]:
+        mv=max(candies)
+        minv=mv-extraCandies
+        ans=[]
+        for i in range(len(candies)):
+            if(minv<=candies[i]):
+                ans.append(True)
+            else:
+                ans.append(False)
+        return ans
