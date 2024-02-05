42fb853890568820341eb947e64fc7710fc3c009
sai-sandeep-gollamudi <107009354+sai-sandeep-gollamudi@users.noreply.github.com>
2024-02-05 00:06:59 -0600

    Update daily_problem.py

--- daily_problem.py
+++ daily_problem.py
@@ -8,3 +8,19 @@ def divideArray(self, nums: List[int], k: int) -> List[List[int]]:
         ans.append([nums[i], nums[i + 1], nums[i + 2]])
     return ans
 
+---02/05---
+    def firstUniqChar(self, s: str) -> int:
+        hm=Counter()
+        for i in range(len(s)):
+            if s[i] in hm:
+                hm[s[i]]=100001
+            else:
+                hm[s[i]]=i
+        ans=[]
+        for k,v in hm.items():
+            if v!=100001:
+                ans.append(v)
+        if len(ans)==0:
+            return -1
+        else:
+           return min(ans)
